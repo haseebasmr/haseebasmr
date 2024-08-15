32cb3b9fe803b9279c4180ddce538e5192fbaa67
Sanaullah-Turab <sanaullahmalik2003@gmail.com>
2024-08-15 18:38:43 +0500

    fix image div

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,18 +1,33 @@
 export default function AboutMe() {
   return (
-    <div className="w-screen bg-white h-screen">
-      <div className=" mx-48 py-24 items-center  h-full">
-        <h1 className="text-7xl font-medium  ">
+    <div className="w-screen bg-white">
+      <div className="md:mx-48 mx-10 py-24">
+        <h1 className="text-7xl font-medium">
           Helping brands thrive <br /> in the digital world
         </h1>
-        <div className="flex mt-20 p-20 gap-14  ">
-          <p className="bg-green-500">
+        <div className="flex md:flex-row flex-col mt-24 pt-24 border-t-2 border-gray-200 md:space-x-8 h-screen md:space-y-0 space-y-8">
+          <span
+            className="max-h-14 font-medium "
+            style={{
+              flex: 1,
+            }}
+          >
             I help companies from all over the world with tailor-made solutions.
             With each project, I push my work to new horizons, always putting
             quality first.
-          </p>
-          <div className="bg-blue-700">
-            <img src="/assets/images/backgroundPic.jpg" alt="no image" />
+          </span>
+
+          <div
+            className=""
+            style={{
+              flex: 2,
+            }}
+          >
+            <img
+              src="/assets/images/backgroundPic.jpg"
+              alt="no image"
+              className="object-cover h-full"
+            />
           </div>
         </div>
       </div>
