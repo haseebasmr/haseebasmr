fb9f7db989a61787714ae11eedaaa4b281d6ab8d
Sanaullah-Turab <sanaullahmalik2003@gmail.com>
2024-08-15 17:33:10 +0500

    add about me page

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,3 +1,21 @@
 export default function AboutMe() {
-  return <h1>Yo this is haseeb niqqa</h1>;
+  return (
+    <div className="w-screen bg-white h-screen">
+      <div className=" mx-48 py-24 items-center  h-full">
+        <h1 className="text-7xl font-medium  ">
+          Helping brands thrive <br /> in the digital world
+        </h1>
+        <div className="flex mt-20 p-20 gap-14  ">
+          <p className="bg-green-500">
+            I help companies from all over the world with tailor-made solutions.
+            With each project, I push my work to new horizons, always putting
+            quality first.
+          </p>
+          <div className="bg-blue-700">
+            <img src="/assets/images/backgroundPic.jpg" alt="no image" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
