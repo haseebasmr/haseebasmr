ce5319418dda00fa7b8b6e6a16f3ad9b810ab669
sanaullah-turab <sanaullahmalik2003@gmail.com>
2024-08-23 23:59:31 +0500

    complete contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,15 +4,21 @@ import { Magnetic } from "./";
 export default function Contact() {
   return (
     <div className="w-screen h-screen bg-pallet1">
-      <div className="pt-44 pl-44 flex  ">
-        <div className="flex-3">
-          <h1 className="text-7xl mt-12 text-white">
+      <div className="md:pt-44 md:pl-44  md:flex-row  flex flex-col-reverse pt-44  p-5">
+        <div className="flex-3   ">
+          <h1 className="text-7xl md:block hidden pt-14 mt-12 pb-6 text-white">
             Let's start a <br /> project together
           </h1>
+          <h1 className="md:hidden block mr-32 text-white text-2xl pb-6 pl-8 ">
+            Let's <br />
+            start a <br />
+            project <br />
+            together
+          </h1>
         </div>
-        <div className="flex-1">
+        <div className="flex-1   md:-mb-0 -mb-24 pt-9  pl-36 md:pl-0 ">
           <img
-            className="object-cover ml-10 h-64 w-64 "
+            className="object-cover md:ml-10 md:h-64 md:w-64 h-24 w-24"
             style={{
               borderRadius: "50%",
             }}
@@ -21,19 +27,19 @@ export default function Contact() {
           />
         </div>
       </div>
-      <div className="flex justify-center gap-8 mt-16">
+      <div className="flex justify-center md:gap-8 md:mt-16">
         <Magnetic>
-          <button className="text-white font-montserrat text-2xl p-2 mx-1 font-medium">
+          <button className="text-white font-montserrat md:text-2xl p-2 mx-1 font-medium">
             Instagram
           </button>
         </Magnetic>
         <Magnetic>
-          <button className="text-white font-montserrat text-2xl p-2 mx-1 font-medium">
+          <button className="text-white font-montserrat md:text-2xl p-2 mx-1 font-medium">
             Facebook
           </button>
         </Magnetic>
         <Magnetic>
-          <button className="text-white font-montserrat text-2xl p-2 mx-1 font-medium">
+          <button className="text-white font-montserrat md:text-2xl p-2 mx-1 font-medium">
             Whatsapp
           </button>
         </Magnetic>
