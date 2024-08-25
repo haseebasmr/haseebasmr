004fd71b35cacb4d9df9b6ed0ac401620cedf2aa
asad-ullah-turab <asadullahturab47@gmail.com>
2024-08-25 13:39:24 +0500

    add food vertical slides to array

--- src/components/photoCategories/Food.jsx
+++ src/components/photoCategories/Food.jsx
@@ -35,6 +35,13 @@ export default function Food() {
       });
     }
   }, []);
+
+  const slides = [
+    { id: 1, image: slide_image_1 },
+    { id: 2, image: slide_image_2 },
+    { id: 3, image: slide_image_3 },
+    { id: 4, image: slide_image_4 },
+  ];
   return (
     <div className="flex items-center justify-center bg-pallet2">
       <div className="text-8xl text-white ml-48 hidden lg:block">Food 🍲</div>
@@ -57,61 +64,19 @@ export default function Food() {
         speed={800}
         className="mySwiper w-screen h-screen px-2 flex-1"
       >
-        <SwiperSlide>
-          <div className="flex justify-center items-center w-full h-full slide-content transition-transform duration-1000">
-            <div
-              className="md:w-[600px] w-[400px] aspect-[4/5] bg-black"
-              style={{
-                backgroundImage: `url(${slide_image_1})`,
-                backgroundSize: "cover",
-              }}
-            ></div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="flex justify-center items-center w-full h-full slide-content transition-transform duration-1000">
-            <div
-              className="md:w-[600px] w-[400px] aspect-[4/5] bg-black"
-              style={{
-                backgroundImage: `url(${slide_image_2})`,
-                backgroundSize: "cover",
-              }}
-            ></div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="flex justify-center items-center w-full h-full slide-content transition-transform duration-1000">
-            <div
-              className="md:w-[600px] w-[400px] aspect-[4/5] bg-black"
-              style={{
-                backgroundImage: `url(${slide_image_3})`,
-                backgroundSize: "cover",
-              }}
-            ></div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="flex justify-center items-center w-full h-full slide-content transition-transform duration-1000">
-            <div
-              className="md:w-[600px] w-[400px] aspect-[4/5] bg-black"
-              style={{
-                backgroundImage: `url(${slide_image_4})`,
-                backgroundSize: "cover",
-              }}
-            ></div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="flex justify-center items-center w-full h-full slide-content transition-transform duration-1000">
-            <div
-              className="md:w-[600px] w-[400px] aspect-[4/5] bg-black"
-              style={{
-                backgroundImage: `url(${slide_image_4})`,
-                backgroundSize: "cover",
-              }}
-            ></div>
-          </div>
-        </SwiperSlide>
+        {slides.map((slide, index) => (
+          <SwiperSlide key={index}>
+            <div className="flex justify-center items-center w-full h-full slide-content transition-transform duration-1000">
+              <div
+                className="md:w-[600px] w-[400px] aspect-[4/5] bg-black"
+                style={{
+                  backgroundImage: `url(${slide.image})`,
+                  backgroundSize: "cover",
+                }}
+              ></div>
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
