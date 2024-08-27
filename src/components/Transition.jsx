b1e8b612c93c36a55aebc151591df74c6bd0cdf7
asad-ullah-turab <asadullahturab47@gmail.com>
2024-08-27 18:34:01 +0500

    add svg

--- src/components/Transition.jsx
+++ src/components/Transition.jsx
@@ -1,4 +1,26 @@
+import { useEffect, useState } from "react";
+
 export default function Transition({ children, text }) {
+  const [dimensions, setDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  const { width, height } = dimensions;
+
   const initialPath = `
     M0 300 
     Q${width / 2} 0 ${width} 300
@@ -13,5 +35,23 @@ export default function Transition({ children, text }) {
     Q${width / 2} ${height} 0 ${height}
     L0 0 `;
 
-  return <div>{children}</div>;
+  return (
+    <div>
+      <svg
+        style={{
+          width: "100vw",
+          height: "calc(100vh + 600px)",
+          top: "-300px",
+          left: "0",
+          position: "fixed",
+          pointerEvents: "none",
+          zIndex: "100",
+        }}
+        fill="blue"
+      >
+        <path d={initialPath}></path>
+      </svg>
+      {children}
+    </div>
+  );
 }
