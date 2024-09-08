20b3796407985eb00f1edff25d1f904cd8527a48
sanaullah-turab <sanaullahmalik2003@gmail.com>
2024-09-08 11:41:44 +0500

    fix navbar shit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -40,7 +40,7 @@ export default function Header() {
   };
 
   return (
-    <header className="flex justify-between bg-transparent items-center md:px-8 md:py-8 py-4 px-2 relative z-40">
+    <header className="flex justify-between absolute w-screen bg-transparent items-center md:px-8 md:py-8 py-4 px-2  z-40">
       <Magnetic>
         <button
           className="text-white font-montserrat text-xl p-2 font-medium"
