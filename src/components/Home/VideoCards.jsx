ce6b561f54ef06895d4ed66cdb62fecbd17efdf1
asad-ullah-turab <asadullahturab47@gmail.com>
2025-09-13 20:02:16 +0500

    Add videos support in stack

--- src/components/Home/VideoCards.jsx
+++ src/components/Home/VideoCards.jsx
@@ -1,37 +1,31 @@
 import { Stack } from "../";
 
-// import slide_video_1 from "/assets/videos/Promotion/thumbnail.mp4";
-// import slide_video_2 from "/assets/videos/Nature/Thumbnail.mp4";
-// import slide_video_3 from "/assets/videos/ASMR/thumbnail.mp4";
-// import slide_video_4 from "/assets/videos/CookingASMR/thumbnail.mp4";
-
-import slide_image_1 from "/assets/images/product1.jpg";
-import slide_image_2 from "/assets/images/food1.jpg";
-import slide_image_3 from "/assets/images/portrait1.jpg";
-import slide_image_4 from "/assets/images/aesthetic1.jpg";
-import slide_image_5 from "/assets/images/nature1.jpg";
+import slide_video_1 from "/assets/videos/Promotion/thumbnail.mp4";
+import slide_video_2 from "/assets/videos/Nature/Thumbnail.mp4";
+import slide_video_3 from "/assets/videos/ASMR/thumbnail.mp4";
+import slide_video_4 from "/assets/videos/CookingASMR/thumbnail.mp4";
 
 function VideoCards() {
   const images = [
     {
       id: 1,
-      img: slide_image_1,
+      src: slide_video_1,
+      type: "video",
     },
     {
       id: 2,
-      img: slide_image_2,
+      src: slide_video_2,
+      type: "video",
     },
     {
       id: 3,
-      img: slide_image_3,
+      src: slide_video_3,
+      type: "video",
     },
     {
       id: 4,
-      img: slide_image_4,
-    },
-    {
-      id: 5,
-      img: slide_image_5,
+      src: slide_video_4,
+      type: "video",
     },
   ];
 
